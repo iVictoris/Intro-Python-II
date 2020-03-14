264d1437419108e85d22b23ad3774a3a6ba714e4
victor tran <victor.tran@tempestas.io>
2020-03-14 00:25:42 -0400

    extend Game into Player class

--- src/player.py
+++ src/player.py
@@ -1,9 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from game import Game
 
 
-class Player:
+class Player(Game):
     def __init__(self, name):
+        super().__init__(name)
         self.location = 'Outside'
         self.bag = []
-        self.name = name
