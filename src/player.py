6e34291bc56f7d9a346b2b9a90f73d7c91d4c450
victor tran <victor.tran@tempestas.io>
2020-03-15 18:07:51 -0400

    update attributes as private and methods to loot, and set/get location

--- src/player.py
+++ src/player.py
@@ -6,5 +6,17 @@ from game import Game
 class Player(Game):
     def __init__(self, name):
         super().__init__(name)
-        self.location = 'Outside'
-        self.bag = []
+        self.__location = 'outside'
+        self.__bag = []
+
+    def loot_items(self, room):
+        self.bag.append(room.items)
+        room.clear_items()
+
+    def _set_location(self, location):
+        self.__location = location
+
+    def _get_location(self):
+        return self.__location
+
+    location = property(_get_location, _set_location)
