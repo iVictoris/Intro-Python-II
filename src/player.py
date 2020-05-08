716476ec6798bf5214b4a1ae85efc215a6e0c4d2
Victor <victor.tran@tempestas.io>
2020-05-07 23:54:15 -0400

    add a str method for player

--- src/player.py
+++ src/player.py
@@ -3,5 +3,8 @@
 
 class Player: 
   def __init__(self, name, room):
-    self.name = name
-    self.current_room = room
+    self.__name = name
+    self.__current_room = room
+
+  def __str__(self):
+    return f'Player: {self.__name}, currently in room: {self.__current_room}'
