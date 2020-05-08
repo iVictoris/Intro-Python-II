dd8187477be562ff7aaf6eca27fcef14f24f8d99
Victor <victor.tran@tempestas.io>
2020-05-07 23:40:14 -0400

    create class Room with init constructor

--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+  def __init__(self, name, description):
+    self.__name = name
+    self.__description = description
+    self.__n = None
+    self.__e = None
+    self.__w = None
+    self.__s = None
