e4402b68bd9bb54f90724a9d501a66e9c4dc7ed6
Victor <victor.tran@tempestas.io>
2020-05-08 17:12:59 -0400

    add on_take and on_drop methods

--- src/item.py
+++ src/item.py
@@ -7,3 +7,10 @@ class Item:
 
   def __repr__(self):
     return f"Item({self.__name})"
+
+  def on_take(self):
+    print(f'You have picked up {self.__name}')
+  
+  def on_drop(self):
+    print(f'You have dropped {self.__name}')
+    
